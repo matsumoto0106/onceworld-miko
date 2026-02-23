8f68a8477b1dc121866177c9a6ea76361cb9ad2d
mikotorei <leiaamaya0@gmail.com>
2026-02-23 16:53:04 +0900

    Update index.MonsterData.js

--- layouts/index.MonsterData.js
+++ layouts/index.MonsterData.js
@@ -1,25 +1,26 @@
 window.MONSTERS = [
 {{- $monsters := where .Site.RegularPages "Section" "monster" -}}
-{{- range sort $monsters "Params.id" }}
+{{- $sorted := sort $monsters "Params.id" -}}
+{{- range $i, $m := $sorted }}
 {
-  id: "{{ .Params.id }}",
-  title: "{{ .Params.title }}",
-  element: "{{ .Params.element }}",
-  attack_type: "{{ .Params.attack_type }}",
-  attack_range: "{{ .Params.attack_range }}",
-  level_shortcuts: {{ .Params.level_shortcuts | jsonify }},
-  exp: {{ .Params.exp }},
-  gold: {{ .Params.gold }},
-  capture_rate: {{ .Params.capture_rate }},
-  drops: {{ .Params.drops | jsonify }},
-  locations: {{ .Params.locations | jsonify }},
-  vit: {{ .Params.status.vit }},
-  spd: {{ .Params.status.spd }},
-  atk: {{ .Params.status.atk }},
-  int: {{ .Params.status.int }},
-  def: {{ .Params.status.def }},
-  mdef: {{ .Params.status.mdef }},
-  luk: {{ .Params.status.luk }}
-}{{ if not (eq (add 1 $index) (len $monsters)) }},{{ end }}
+  id: "{{ $m.Params.id }}",
+  title: "{{ $m.Params.title }}",
+  element: "{{ $m.Params.element }}",
+  attack_type: "{{ $m.Params.attack_type }}",
+  attack_range: "{{ $m.Params.attack_range }}",
+  level_shortcuts: {{ $m.Params.level_shortcuts | jsonify }},
+  exp: {{ $m.Params.exp }},
+  gold: {{ $m.Params.gold }},
+  capture_rate: {{ $m.Params.capture_rate }},
+  drops: {{ $m.Params.drops | jsonify }},
+  locations: {{ $m.Params.locations | jsonify }},
+  vit: {{ $m.Params.status.vit }},
+  spd: {{ $m.Params.status.spd }},
+  atk: {{ $m.Params.status.atk }},
+  int: {{ $m.Params.status.int }},
+  def: {{ $m.Params.status.def }},
+  mdef: {{ $m.Params.status.mdef }},
+  luk: {{ $m.Params.status.luk }}
+}{{ if lt (add $i 1) (len $sorted) }},{{ end }}
 {{- end }}
 ];
